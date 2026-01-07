fdd7e0d9137540ee8030c4cfe06688691fc2f78e
zzj <newutopia@163.com>
2026-01-07 16:49:16 +0800

    fix: 修复后台系统403错误和点击退出登录时，不会跳转到登录页面的错误

--- admin-system/src/components/RightContent/AvatarDropdown.tsx
+++ admin-system/src/components/RightContent/AvatarDropdown.tsx
@@ -59,7 +59,7 @@ export const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({
     /** 此方法会跳转到 redirect 参数所在的位置 */
     const redirect = urlParams.get('redirect');
     // Note: There may be security issues, please note
-    if (window.location.pathname !== '/user/login' && !redirect) {
+    if (window.location.pathname !== '/user/login') {
       history.replace({
         pathname: '/user/login',
         search: searchParams.toString(),
