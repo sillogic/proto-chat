7690bd1ac13656e6656f8ecd886496dcd70dcfec
Arvin Xu <arvinx@foxmail.com>
2026-03-01 12:19:10 +0800

    🔨 chore: fix oidc-provider issue in turbopack (#12550)
    fix oidc-provider issue
    
    (cherry picked from commit 8bc3a8a886e43ed7784baf033fd9ea64fe14abef)

--- src/libs/next/config/define-config.ts
+++ src/libs/next/config/define-config.ts
@@ -80,8 +80,6 @@ export function defineConfig(config: CustomNextConfig) {
       // refs: https://github.com/lobehub/lobe-chat/pull/7430
       serverMinification: false,
       webVitalsAttribution: ['CLS', 'LCP'],
-      webpackBuildWorker: true,
-      webpackMemoryOptimizations: true,
       ...config.experimental,
     },
     async headers() {
@@ -357,16 +355,19 @@ export function defineConfig(config: CustomNextConfig) {
       'pdfkit',
       '@napi-rs/canvas',
       'pdfjs-dist',
+      'ajv',
+      'oidc-provider',
     ],
 
     transpilePackages: ['mermaid', 'better-auth-harmony'],
     turbopack: {
-      rules: isTest || !enableCodeInspector
-        ? void 0
-        : codeInspectorPlugin({
-            bundler: 'turbopack',
-            hotKeys: ['altKey', 'ctrlKey'],
-          }),
+      rules:
+        isTest || !enableCodeInspector
+          ? void 0
+          : codeInspectorPlugin({
+              bundler: 'turbopack',
+              hotKeys: ['altKey', 'ctrlKey'],
+            }),
       ...config.turbopack,
     },
 
